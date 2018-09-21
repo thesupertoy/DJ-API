be87bc38c5b95ae71529e7c3a69357b1ab372b6f
Craig Morgen <craig.morgen@target.com>
2018-09-20 21:53:41 -0500

    First Commit

---
+++ src/main/java/com/DJ/DJApplication.java
@@ -0,0 +1,11 @@
+package com.DJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DJApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DJApplication.class, args);
+    }
+}
